0e865c2b7b7814bb5ffe5ba3ebd7f038d2cc81fc
saidatta1 <b118051@iiit-bh.ac.in>
2020-06-22 16:48:07 +0530

    mobile view changes

--- comingsoonphp/index.php
+++ comingsoonphp/index.php
@@ -52,7 +52,7 @@
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
     />
-    <link rel="stylesheet" href="style.css?v=1.1" />
+    <link rel="stylesheet" href="style.css?v=1.24" />
   </head>
   <body>
     <div class="container">
